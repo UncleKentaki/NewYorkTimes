046b0bdcf0d2da349c5b7a9a25e6f5243735ede7
Sean Naylor <Sean Naylor>
2017-03-04 22:44:07 -0800

    gettin closer

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,58 @@
 // When user presses search get data
-    $("#searchButton").on("click", function(){
-        // Get user input
-        var searchKey = $("#search").val().trim();
-        var numRecords = $("#numRecords").val();
-        var startYear = $("#startYear").val();
-        var endYear = $("#endYear").val();
+$("#searchButton").on("click", function () {
+    $("#topArticles").empty();
+    $("#searchBox").val("");
+    $("#startYear").val("");
+    $("#endYear").val("");
+    
+    // Get user input
+    var searchKey = $("#searchBox").val().trim();
+    var numRecords = $("#numRecords").val();
+    var startYear = $("#startYear").val();
+    var endYear = $("#endYear").val();
 
-        var authKey = "b9f91d369ff59547cd47b931d8cbc56b:0:74623931";
-        var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + authKey + "&q=" + searchKey + "";
-        queryURL = encodeURI(queryURL);
-
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).done(function(response) {
-            console.log(response);
-            results = response.response.docs;
-            console.log(results);
-
-            for(var i = 0; i<results.length; i++){
-                var newDiv = $("<div>");
-                var title = $("<h2>").text(results[i].headline.main);
-                var author = $("<p>").text(results[i].byline);
-                var weburl = $("<p>").text(results[i].web_url);
-                newDiv.append(title);
-                newDiv.append(author);
-                newDiv.append(weburl);
-
-                $("#topArticles").append(newDiv);
-            }
-
-
-        });
+   
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    url += '?' + $.param({
+        'api-key': "a5dc5060a9b341948c54cd5ba1232a39",
+        'q': searchKey
+        
     });
+
+console.log(url);
+
+    
+    $.ajax({
+        url: url,
+        method: 'GET',
+        
+    }).done(function (response) {
+        console.log(response);
+        var result = response.response.docs;
+        console.log(result);
+         for (var i = 0; i < numRecords; i++) {
+        var newDiv = $("<div>");
+        var title = $("<h2>").text(result[i].headline.main);
+        var author = $("<p>").text(result[i].byline.original);
+        var weburl = $("<p>").text(result[i].web_url);
+        newDiv.append(title);
+        newDiv.append(author);
+        newDiv.append(weburl);
+
+        $("#topArticles").append(newDiv);
+    }
+
+    }).fail(function (err) {
+        throw err;
+    });
+        
+   
+
+});
+
+$("#clearResults").on("click", function(){
+    $("#topArticles").empty();
+    $("#searchBox").val("");
+    $("#startYear").val("");
+    $("#endYear").val("");
+});
